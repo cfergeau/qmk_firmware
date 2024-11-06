971a65290ebfd3545a9858abdd87f865b281bc05
Christophe Fergeau <cfergeau@redhat.com>
2024-11-06 10:22:58 +0100

    planck: Add Shift+Ralt to symbols layer
    This will be needed for compatibility with AltGr in ergo-l, as this
    allows to input some accent-related dead keys.

--- keyboards/planck/rev7/keymaps/basic/keymap.c
+++ keyboards/planck/rev7/keymaps/basic/keymap.c
@@ -25,6 +25,7 @@ enum planck_keycodes { QWERTY = SAFE_RANGE, DVORAK };
 
 #define SPC_NAV LT(_NAVIM, KC_SPC)
 #define CTL_ESC LCTL_T(KC_ESC)
+#define SFT_RALT LSFT(KC_RALT)
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -101,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     _______, KC_CIRC, KC_LT,   KC_GT,   KC_DLR,  KC_PERC, KC_AT,   KC_AMPR, KC_ASTR, KC_QUOT, KC_GRV,  KC_BSPC,
     _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_EQL,  KC_BSLS, KC_PLUS, KC_MINS, KC_SLSH, KC_DQT,  _______,
-    _______, KC_TILD, KC_LBRC, KC_RBRC, KC_UNDS, KC_HASH, KC_PIPE, KC_EXLM, KC_SCLN, KC_COLN, KC_QUES, _______,
+    SFT_RALT,KC_TILD, KC_LBRC, KC_RBRC, KC_UNDS, KC_HASH, KC_PIPE, KC_EXLM, KC_SCLN, KC_COLN, KC_QUES, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
