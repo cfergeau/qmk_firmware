93cacdbaf0ec0959421a923097e887e4482b24e4
Christophe Fergeau <cfergeau@redhat.com>
2024-11-08 17:28:22 +0100

    planck: Allow to change unicode input method
    How to input unicode characters is OS-dependent.
    This adds 3 prev/next/linux keys to the ADJUST layer.

--- keyboards/planck/rev7/keymaps/basic/keymap.c
+++ keyboards/planck/rev7/keymaps/basic/keymap.c
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |MUSmod|Aud on|Audoff|      |      |Qwerty|      |Dvorak|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|      |      |      |      |      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|      |      |UniPrv|UniNxt|UniLnx|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT, DB_TOGG, UG_TOGG, UG_NEXT, UG_HUEU, UG_HUED, UG_SATU, UG_SATD, UG_SPDU, UG_SPDD, KC_DEL ,
     _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  _______, _______, QWERTY,  XXXXXXX, DVORAK,  _______, _______,
-    _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  _______, _______, _______, _______, _______, _______, _______,
+    _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  _______, _______, UC_PREV, UC_NEXT, UC_LINX, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
