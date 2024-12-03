c0dc23a905bdf0b0b2734c9e8917284af795d425
Christophe Fergeau <cfergeau@redhat.com>
2024-12-03 10:16:17 +0100

    planck: Shift vim nav one row to the left
    This matches 'real' vim keybindings, and leave space for del/backspace
    to the right.

--- keyboards/planck/rev7/keymaps/basic/keymap.c
+++ keyboards/planck/rev7/keymaps/basic/keymap.c
@@ -163,19 +163,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Navigation/vim layer
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |      |   7  |   8  |   9  |      |      | Home | PgDn | PgUp | End  | Bksp |
+ * | Tab  |      |   7  |   8  |   9  |      | Home | PgDn | PgUp | End  | Del  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   0  |   4  |   5  |   6  |   0  |      | Left | Down |  Up  |Right |  '   |
+ * | Esc  |   0  |   4  |   5  |   6  |   0  | Left | Down |  Up  |Right | Bksp |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|      |   1  |   2  |   3  |      |      | Home | PgDn | PgUp | End  |Enter |
+ * | Shift|      |   1  |   2  |   3  |      | Home | PgDn | PgUp | End  | Del  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |RShift| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_NAVIM] = LAYOUT_planck_grid(
-    KC_TAB,  XXXXXXX, KC_7,    KC_8,    KC_9,  XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
-    KC_ESC,  KC_0,    KC_4,    KC_5,    KC_6,  KC_0,    XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_LSFT, XXXXXXX, KC_1,    KC_2,    KC_3,  XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT ,
+    KC_TAB,  XXXXXXX, KC_7,    KC_8,    KC_9,  XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,  KC_BSPC,
+    KC_ESC,  KC_0,    KC_4,    KC_5,    KC_6,  KC_0,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, KC_QUOT,
+    KC_LSFT, XXXXXXX, KC_1,    KC_2,    KC_3,  XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,  KC_ENT ,
     KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER, XXXXXXX, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
